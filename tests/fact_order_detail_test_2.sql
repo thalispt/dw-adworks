e6de2d195249d2aac690a6cdd558259c6f9c883e
Thalis Pasolini Trabach <thalis.trabach@gmail.com>
2022-08-31 01:47:43 +0000

    Adding yml descriptions and test's changes.

--- tests/fact_order_detail_test_2.sql
+++ tests/fact_order_detail_test_2.sql
@@ -1,7 +1,7 @@
 with validation as (
     select
-        sum(unitprice) as total
+        cast(sum(unitprice) as int) as total
     from {{ ref ('fact_order_detail') }}
     where orderdate between '2012-12-01' and '2012-12-31' )
 
-select * from validation where total != 1257847.1713999873
+select * from validation where total != 1257847
